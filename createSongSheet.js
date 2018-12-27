7d778f828fd7b5e8fffe26ce2b8afff39c15e708
bf123 <1334427590@qq.com>
2018-12-27 17:37:21 +0800

    sdf

--- createSongSheet.js
+++ createSongSheet.js
@@ -5,13 +5,16 @@ function createSongSheet(data,who){
         let a = document.createElement("a");
 				a.href = "javascript:;";
                 a.title = "查看详情";
-                a.style.cssText = "text-overflow: ellipsis;overflow: hidden;white-space: nowrap;display:block;height:40px;border:1px solid black;border-top:none;box-sizing:border-box;line-height:40px"
+                a.style.cssText = 
+                "text-overflow: ellipsis;overflow: hidden;white-space: nowrap;display:block;height:40px;border:1px solid black;border-top:none;"+
+                "box-sizing:border-box;line-height:40px"
         let content = document.createElement("li");
             a.name = sheet.id; 
             a.textContent =sheet.name;
+            content.style.backgroundColor = "rgba(0,200,50,.2)"
             content.appendChild(a);
             who.appendChild(content);
-            content.onclick = function(){
+            a.onclick = function(){
                 // let url = `https://api.bzqll.com/music/tencent/url?key=579621905&id=${this.name}&br=192`;
                 // playSong(url);
             }
